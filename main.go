e3f58b04e2177e844d33c51bf79e8100b6b97288
hwan20c <hwan20c@gmail.com>
2024-01-10 00:24:46 +0900

    fuction 1

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
-	name := "nico"
-	name = "lynn"
-	fmt.Println(name)
+	repeatMe("nico", "lynn", "dal", "marl", "flynn")
 }
