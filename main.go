55e03261bb84057bb5c5b8e00ce156385e18a0a3
hwan20c <hwan20c@gmail.com>
2024-01-10 00:42:58 +0900

    loop of go

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func lenAndUpper(name string) (length int, upppercase string) {
-	defer fmt.Println("I'm done.")
-	length = len(name)
-	upppercase = strings.ToUpper(name)
-	return
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
 }
 
 func main() {
-	totalLength, up := lenAndUpper("nico")
-	fmt.Println(totalLength, up)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
