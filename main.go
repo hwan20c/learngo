f93bcc4677956776ab4a4f751564bf97e0c7d3f0
hwan20c <hwan20c@gmail.com>
2024-01-10 00:53:10 +0900

    switch of go

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import "fmt"
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
+	switch {
+	case age < 18:
+		return false
+	case age >= 18:
+		return true
+	case age > 50:
 		return false
 	}
-	return true
+	return false
 }
 
 func main() {
-	fmt.Println(canIDrink(16))
+	fmt.Println(canIDrink(20))
 }
