9ea2cb468964851deb1b3f61d763a1de916932e7
hwan20c <hwan20c@gmail.com>
2024-01-10 18:09:46 +0900

    Structs of go

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	nico := map[string]string{"name": "nico", "age": "12"}
-	for key, value := range nico {
-		fmt.Println(key, value)
-	} // key나 value를 ignore 할 수 있다.
+	favFood := []string{"kimchi", "ramen"}
+	nico := person{name: "nico", age: 18, favFood: favFood}
+	fmt.Println(nico)
 }
