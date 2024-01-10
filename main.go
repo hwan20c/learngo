338f39b416e2c11ab1e1c287016a4d4e0ead011c
hwan20c <hwan20c@gmail.com>
2024-01-10 17:43:39 +0900

    pointers of go

--- main.go
+++ main.go
@@ -2,18 +2,9 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-	switch {
-	case age < 18:
-		return false
-	case age >= 18:
-		return true
-	case age > 50:
-		return false
-	}
-	return false
-}
-
 func main() {
-	fmt.Println(canIDrink(20))
+	a := 2
+	b := &a
+	*b = 202020
+	fmt.Println(a)
 }
