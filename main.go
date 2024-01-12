880206f6230cd53388d599da3a45474312d0ede0
hwan20c <hwan20c@gmail.com>
2024-01-12 17:11:12 +0900

    update and delete for dictionary

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "First word"}
-	/* search
+
+	/* //search
 	definition, err := dictionary.Search("first")
 	if err != nil {
 		fmt.Println(err)
@@ -15,6 +16,8 @@ func main() {
 		fmt.Println(definition)
 	}
 	*/
+
+	/* //add
 	word := "hello"
 	definition := "Greeting"
 	err := dictionary.Add(word, definition)
@@ -27,4 +30,17 @@ func main() {
 	if err2 != nil {
 		fmt.Println(err2)
 	}
+	*/
+
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(word)
+	}
+
 }
