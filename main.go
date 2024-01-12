b73ba8a8ada640cb0de3bb300797affa50d8cf57
hwan20c <hwan20c@gmail.com>
2024-01-12 17:00:00 +0900

    add a dictionary ADD

--- main.go
+++ main.go
@@ -7,10 +7,24 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "First word"}
+	/* search
 	definition, err := dictionary.Search("first")
 	if err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Println(definition)
+	}
+	*/
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
+	if err != nil {
+		fmt.Println(err)
+	}
+	hello, _ := dictionary.Search(word)
+	fmt.Println("found :", word, " definition :", hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
 	}
 }
