ecd75915ec0bc97498852d6a1b55f7234599ab93
hwan20c <hwan20c@gmail.com>
2024-01-16 15:03:15 +0900

    URL Checker upgrade

--- main.go
+++ main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 )
 
-type result struct {
+type RequestResult struct {
 	url    string
 	status string
 }
 
-var errReqeustFailed = errors.New("Request failed")
-
 func main() {
-	c := make(chan result)
+	results := make(map[string]string)
+	c := make(chan RequestResult)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -30,14 +28,23 @@ func main() {
 	for _, url := range urls {
 		go hitURL(url, c)
 	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+
 }
 
-func hitURL(url string, c chan<- result) {
-	fmt.Println("Checking:", url)
+func hitURL(url string, c chan<- RequestResult) {
 	resp, err := http.Get(url)
 	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
 		status = "FAILED"
 	}
-	c <- result{url: url, status: status}
+	c <- RequestResult{url: url, status: status}
 }
