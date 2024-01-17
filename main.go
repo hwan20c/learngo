a15da85203079dbc3b0aab753b093fb97cb040b3
hwan20c <hwan20c@gmail.com>
2024-01-17 16:33:25 +0900

    bring a Element

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+}
+
 var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
 
 func main() {
@@ -21,6 +27,26 @@ func main() {
 func getPage(page int) {
 	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page+1)
 	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".item_recruit")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("value")
+		fmt.Println(id)
+		title := card.Find(".area_job>h2>a").Text()
+		fmt.Println(title)
+		location := card.Find(".area_job>.job_condition>span>a").Text()
+		fmt.Println(location)
+	})
+
 }
 
 func getPages() int {
@@ -52,4 +78,8 @@ func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Request failed with Status:", res.StatusCode)
 	}
+}
+
+func cleanString(str string) {
+
 }
